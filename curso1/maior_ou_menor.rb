85c1ee85e64ee1979a19f1a9267824bc9cdf68d3
Icaro Florencio <icarooflorencio@gmail.com>
2016-12-18 14:44:44 -0200

    Refatoracao do codigo

--- curso1/maior_ou_menor.rb
+++ curso1/maior_ou_menor.rb
@@ -1,28 +1,60 @@
-puts "Bem-vindo ao jogo de adivinhação!\n"
-puts "\nDigite o seu nome:"
-nome = gets
-puts "\nO jogo vai começar, " + nome + "\n"
-puts "Escolhendo um número secreto entre 0 e 200...\n\n"
-numero_secreto = 123
-puts "Escolhido... Adivinhe o número"
-acertou = false
-
-while !acertou
-	numero = gets
-	puts "\nVocê escolheu o número " + numero + ". Será que acertou?\n\n"
-
-	acertou = numero_secreto == numero.to_i
-	if acertou
-		puts "Acertou!"
-	else 
-		puts "Errou"
-
-		if numero_secreto < numero.to_i
-			puts "O número é menor que " + numero
-		else
-			puts "O número é maior que " + numero
-		end
-
-		puts "Tente novamente"
-	end
+def boasVindas
+    puts "Bem-vindo ao jogo de adivinhação!\n"
+    puts "\nDigite o seu nome:"
+    nome = gets.strip
+    puts "\nO jogo vai começar, #{nome}\n\n" 
 end
+
+def sorteiaNumero
+    puts "Escolhendo um número secreto entre 0 e 200...\n"
+    numero = (rand * 100).round
+    puts "Escolhido... Adivinhe o número ou digite -1 para sair\n\n"
+    numero
+end
+ 
+def chutaNumero (chutes, tentativa)
+    puts "Tentativa #{tentativa}\n"
+    puts "Chutes dados até o momento: #{chutes}\n\n"
+    
+    gets.strip.to_i
+end
+
+def adivinha (numeroSecreto, chute)
+    acertou = numeroSecreto == chute
+    
+	if acertou
+		puts "Acertou, Mizerávi!\n\n"
+	else
+        if(chute == -1)
+            puts "Desistiu? Seu pato!"
+        else
+            puts "Errou!\n\n"
+
+            if numeroSecreto < chute
+                puts "O número é menor que #{chute}"
+            else
+                puts "O número é maior que #{chute}"
+            end
+
+            puts "Tente novamente\n\n"
+        end
+	end
+    
+    acertou
+end
+
+boasVindas
+numeroSecreto = sorteiaNumero
+
+tentativa = 1
+acertou = false
+chutes = []
+
+while !acertou && chutes.last != -1
+    chutes << chutaNumero(chutes, tentativa)
+    
+	acertou = adivinha numeroSecreto, chutes.last
+    
+    tentativa += 1
+end
+
