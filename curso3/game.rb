7ec24bb2ba292e1a1e6db369fd47e5b9578ce8ec
= <=>
2017-01-18 22:56:59 -0200

    Adicao de movimento dos hunters e tratamento de colisao com muro e entre eles

--- curso3/game.rb
+++ curso3/game.rb
@@ -42,10 +42,49 @@ def isColision?(charPosition, map)
     map[charPosition[0]][charPosition[1]] == 'X'
 end
 
+def hasHunter?(charPosition, map)
+    map[charPosition[0]][charPosition[1]] == 'H'
+end
+
 def movePlayer(map, direction)
     hero = "P"
     heroPos = findPlayer map, hero
     changePosition heroPos, direction, hero, map
+end
+
+def moveHunter(map, hunterPos, direction, hunter)
+    moves = {
+        0 => [-1, 0],
+        1 => [1, 0],
+        2 => [0, -1],
+        3 => [0, 1]
+    }
+    
+    newPos = hunterPos.dup
+    
+    move = moves[direction]
+    
+    newPos[0] += move[0]
+    newPos[1] += move[1]
+    
+    if !isColision?(newPos, map) && !hasHunter?(newPos, map)
+        map[hunterPos[0]][hunterPos[1]] = " "
+        hunterPos = newPos
+        map[hunterPos[0]][hunterPos[1]] = hunter
+    end
+end
+
+def moveHunters(map)
+    hunter = "H"
+    
+    map.each_with_index do |curLine, line|
+        curLine.chars.each_with_index do |curCol, column|
+            if curCol == hunter
+                direction = rand(4)
+                moveHunter map, [line, column], direction, hunter
+            end
+        end
+    end
 end
 
 def play
@@ -60,7 +99,7 @@ def play
         
         movePlayer map, direction
         
-        moveHunter map
+        moveHunters map
     end
 end
 
